b59c986b24f69876354645d5b15c2cfef42e5bc2
Laharnar <marko.laharnar@gmail.com>
2019-08-03 22:20:52 +0200

    Level 1 - intro, design done

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+    public string loadScene = "";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(loadScene);
+    }
+}
---
+++ Assets/Scripts/SlowMo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SlowMo : MonoBehaviour
+{
+    bool init = false;
+    static int falling = 0;
+    public int numOfCollisionsOnPlayer = 2;
+
+    private void Start()
+    {
+        init = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (init)
+        {
+            // when player hits the trigger, slow down time, then resume it after hitting a different trigger.
+            if (falling == 0)
+            {
+                Debug.Log("Time slowed");
+                falling++;
+                Time.timeScale = 0.3f;
+            }
+            else if (falling >= numOfCollisionsOnPlayer) 
+            {
+                Debug.Log("Time resumed");
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                falling++;
+
+            }
+        }
+    }
+
+    public static void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    public static void Unpause()
+    {
+        Time.timeScale = 1f;
+    }
+}
