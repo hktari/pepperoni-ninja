8502fbba1a49d4ff6c72fedf28fa331acbc3b685
hktari <bkamnik1995@gmail.com>
2019-08-03 17:40:38 +0200

    adds rhytm canvas and manager

---
+++ Assets/RhytmCanvas.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RhytmCanvas : MonoBehaviour
+{
+    public float PanelColorResetTime = 0.25f;
+
+    private float m_colorResetTimer;
+
+    private GameObject backgroundImg;
+    private GameObject panelImg;
+    private RhytmManager.RhytmAction m_curRhytmState;
+    private Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundImg = transform.GetChild(0).gameObject;
+        panelImg = transform.GetChild(1).gameObject;
+        image = panelImg.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        m_colorResetTimer -= Time.deltaTime;
+        if (m_colorResetTimer <= 0.0f)
+        {
+            m_curRhytmState = RhytmManager.RhytmAction.None;
+        }
+
+        Color panelColor = Color.white;
+        switch (m_curRhytmState)
+        {
+            case RhytmManager.RhytmAction.None:
+                panelColor = Color.white;
+                break;
+            case RhytmManager.RhytmAction.Failed:
+                panelColor = Color.red;
+                break;
+            case RhytmManager.RhytmAction.Success:
+                panelColor = Color.green;
+                break;
+            default:
+                break;
+        }
+
+        image.color = panelColor;
+    }
+
+    public void UpdateCanvas(float perc)
+    {
+        Rect backgroundRect = backgroundImg.GetComponent<RectTransform>().rect;
+        var newX = backgroundImg.transform.position.x + backgroundRect.width * perc;
+        var panelRect = panelImg.GetComponent<RectTransform>().rect;
+        panelImg.transform.position = new Vector3(newX, panelImg.transform.position.y, panelImg.transform.position.z);
+    }
+
+    public void DisplayAction(RhytmManager.RhytmAction rhytmAction)
+    {
+        m_colorResetTimer = PanelColorResetTime;
+        m_curRhytmState = rhytmAction;
+    }
+}
---
+++ Assets/Scripts/RhytmManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhytmManager : MonoBehaviour
+{
+    public enum RhytmAction
+    {
+        None,
+        Failed,
+        Success
+    }
+
+    public RhytmCanvas Canvas;
+
+    public float RhytmTimeMax { get; set; } = 1.0f;
+
+    public float CurRhytmTime { get; set; }
+    public float RhytmActionLeewayInSec { get; set; } = 0.1f;
+
+    private bool leftToRight = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftToRight)
+        {
+            CurRhytmTime += Time.deltaTime;
+        }
+        else
+        {
+            CurRhytmTime -= Time.deltaTime;
+        }
+
+        if (CurRhytmTime > RhytmTimeMax || CurRhytmTime < 0.0f)
+        {
+            leftToRight = !leftToRight;
+        }
+
+        Canvas?.UpdateCanvas(CurRhytmTime / RhytmTimeMax);
+    }
+
+    public bool CanPerformAction()
+    {
+        var success = Mathf.Abs(CurRhytmTime - 0.5f) < RhytmActionLeewayInSec;
+        Canvas.DisplayAction(success ? RhytmAction.Success : RhytmAction.Failed);
+        return success;
+    }
+}
