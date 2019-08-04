91942001e3ad16d57c0d61a08404706644317f10
KirillSamoilenko <kirillsamoi@gmail.com>
2019-08-04 12:52:14 +0200

    Adding timeline and level end assets

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class LevelEnd: MonoBehaviour {
+
+    public string loadScene = "";
+    public PlayableDirector track;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        track.Play();
+        SceneManager.LoadScene(loadScene);
+    }
+}
