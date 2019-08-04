a9bba82a98a7288976db33dcb38317b00d46c247
hktari <bkamnik1995@gmail.com>
2019-08-04 18:29:46 +0200

    bar size changes

--- Assets/Scripts/RhytmManager.cs
+++ Assets/Scripts/RhytmManager.cs
@@ -19,14 +19,17 @@ public class RhytmManager : MonoBehaviour
     [SerializeField]
     public float RhytmActionLeewayInSec = 0.1f;
 
+    public float SpeedUpgradeFactor = 0.75f;
+
     public float CurRhytmTime { get; set; }
 
     private bool leftToRight = true;
-    
+
+    private float m_origRhytmTime;
     // Start is called before the first frame update
     void Start()
     {
-
+        m_origRhytmTime = RhytmTimeMax;
     }
 
     // Update is called once per frame
@@ -52,8 +55,17 @@ public class RhytmManager : MonoBehaviour
 
     public bool TryPerformAction()
     {
-        var success = Mathf.Abs(CurRhytmTime - RhytmTimeMax / 2.0f) < RhytmActionLeewayInSec;
-        Debug.Log("SuCCESS: " + success);
+        //var success = Mathf.Abs(CurRhytmTime - RhytmTimeMax / 2.0f) < RhytmActionLeewayInSec;
+        //Debug.Log("SuCCESS: " + success);
+        var success = Canvas.TryExecuteAction();
+        if (success)
+        {
+            RhytmTimeMax *= SpeedUpgradeFactor;
+        }
+        else
+        {
+            RhytmTimeMax = m_origRhytmTime; 
+        }
         Canvas.DisplayAction(success ? RhytmAction.Success : RhytmAction.Failed);
         return success;
     }
