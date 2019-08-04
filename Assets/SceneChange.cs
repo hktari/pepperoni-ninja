10c52f65bef9678e843ab048423c61f22f98058b
KirillSamoilenko <kirillsamoi@gmail.com>
2019-08-04 10:08:38 +0200

    Adding MainMenu

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void DoExitGame()
+    {
+        Debug.Log("You exited the game");
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
